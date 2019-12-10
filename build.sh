0282822fdd00bbbe8c4bc4228d840480585a560a
dimajolkin <dimajolkin@rambler.ru>
2019-12-10 21:16:23 +0200

    optimization travis

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
 FILE=./var/data.sqlite
 if [ ! -f "$FILE" ]; then
     echo "create DB"
-    docker run -u $UID -v $PWD/var:/app -w /app nouchka/sqlite3 data.sqlite
+    docker run -u $UID:$UID -v var:/app -w /app nouchka/sqlite3 data.sqlite
 fi
 
 composer install
-./bin/console doctrine:schema:create
-./bin/console doctrine:migrations:migrate -n
-./bin/console assets:install
 ./vendor/bin/codecept build
+
+./bin/console assets:install
+./bin/console doctrine:schema:drop -f
+./bin/console doctrine:schema:create
+./bin/console doctrine:migrations:migrate prev -n
+./bin/console doctrine:migrations:migrate -n
+./bin/console app:currency-rate-update
